2563364d21833cb0d7974ffb85f14b049b5e2462
Loic Rio <lrio@student.42lyon.fr>
2024-02-27 16:37:23 +0100

    fix < and >

--- srcs/lexer/redirection.c
+++ srcs/lexer/redirection.c
@@ -6,7 +6,7 @@
 /*   By: lrio <lrio@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 15:42:20 by lrio              #+#    #+#             */
-/*   Updated: 2024/02/27 16:08:12 by lrio             ###   ########.fr       */
+/*   Updated: 2024/02/27 16:36:10 by lrio             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "lexer.h"
@@ -20,9 +20,9 @@ int lexer_redirection(enum e_str_type *last, enum e_str_type *type, char *str, i
 		*i +=2 ;
 		*type = get_type(str[*i]);
 	}
-	if (*i != 0 && str[*i + 1] && (str[*i] == '<' && str[*i + 1] != '<'))
-		return (1);
-	if (*i != 0 && str[*i + 1] && (str[*i] == '>' && str[*i + 1] != '>'))
-		return (1);
+//	if (*i != 0 && str[*i + 1] && (str[*i] == '<' && str[*i + 1] != '<'))
+//		return (1);
+//	if (*i != 0 && str[*i + 1] && (str[*i] == '>' && str[*i + 1] != '>'))
+//		return (1);
 	return (0);
 }
