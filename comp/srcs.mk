938345c5c72f7e917a1a937859b45aca716a45ea
Michel Juffard <mjuffard@z2r5p1.42lyon.fr>
2024-02-28 04:51:47 +0100

    create functions logical operator and redirect

--- comp/srcs.mk
+++ comp/srcs.mk
@@ -4,23 +4,28 @@ LEXERDIR = $(SRCDIR)lexer/
 PARSINGDIR = $(SRCDIR)parsing/
 TOOLSDIR = $(SRCDIR)tools/
 TREEDIR = $(TOOLSDIR)tree/
-
+EXECDIR = $(SRCDIR)exec/
 
 SRCPROMT = prompt
+
 SRCLEXER = lexer \
 		   operator \
 		   parenthese \
 		   quote \
 		   get_type \
 		   redirection
+
 SRCPARSING = parsing
+
 SRCTREE =  ft_clean_tree \
 		   ft_join_tree \
 		   ft_new_tree
 
+SRCEXEC = exec
 
 SRCS =  $(SRCDIR)main.c \
 	   $(addprefix $(PROMPTDIR), $(addsuffix .c, $(SRCPROMT))) \
 	   $(addprefix $(PARSINGDIR), $(addsuffix .c, $(SRCPARSING))) \
 	   $(addprefix $(TREEDIR), $(addsuffix .c, $(SRCTREE))) \
-	   $(addprefix $(LEXERDIR), $(addsuffix .c, $(SRCLEXER)))
+	   $(addprefix $(LEXERDIR), $(addsuffix .c, $(SRCLEXER))) \
+	   $(addprefix $(EXECDIR), $(addsuffix .c, $(SRCEXEC)))
--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: mjuffard <mjuffard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 00:16:15 by mjuffard          #+#    #+#             */
-/*   Updated: 2024/02/27 18:03:12 by mjuffard         ###   ########lyon.fr   */
+/*   Updated: 2024/02/28 04:50:13 by mjuffard         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,9 @@
 Executer commande de l'arbre de bas gauche vers droite.
 */
 
+#include "libft.h"
 #include "tree.h"
+#include <fcntl.h>
 
 t_tree	*next_exec(t_tree *tree)
 {
@@ -27,6 +29,13 @@ t_tree	*next_exec(t_tree *tree)
 
 int	exec_args(t_tree *tree)
 {
+	static t_vector	*fd_in = NULL;
+	static t_vector	*fd_out = NULL;
+
+	if (!fd_in)
+		ft_vector_init(fd_in, sizeof(int));
+	if (!fd_in)
+		ft_vector_init(fd_out, sizeof(int));
 	if (tree->type == OPE)
 	{
 		if ((t_operator *)tree->structur == OR)
@@ -34,9 +43,21 @@ int	exec_args(t_tree *tree)
 		if ((t_operator *)tree->structur == AND)
 			exec_and(tree);
 		if ((t_operator *)tree->structur == PIPE)
-			exec_pipe(tree);
+			exec_pipe(tree, fd_in, fd_out);
 	}
 	if (tree->type == REDIRECT)
+	{
+		if ((t_redirect *)tree->structur == HERE_DOC)
+			exec_here_doc(tree);
+		if ((t_redirect *)tree->structur == APPEND)
+			exec_apend(tree, fd_out);
+		if ((t_redirect *)tree->structur == OUTPUT)
+			exec_output(tree, fd_out);
+		if ((t_redirect *)tree->structur == INPUT)
+			exec_input(tree, fd_in);
+	}
+	if (tree->type == CMD)
+		exec_cmd(tree, fd_in, fd_out);
 }
 
 int	exec_or(t_tree *tree)
@@ -59,10 +80,43 @@ int	exec_and(t_tree *tree)
 	return (ret);
 }
 
-int	exec_pipe(t_tree *tree)
+
+int	exec_input(t_tree *tree, t_vector *fd_in)
+{
+	int	fd;
+
+	fd = open((char *)tree->right, O_RDONLY);
+	ft_vector_add(fd_in, fd);
+	exec_args(tree->left);
+}
+
+int	exec_output(t_tree *tree, t_vector *fd_out)
+{
+	int	fd;
+
+	fd = open((char *)tree->right, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	ft_vector_add(fd_out, fd);
+	exec_args(tree->left);
+}
+
+int	exec_apend(t_tree *tree, t_vector *fd_out)
+{
+	int	fd;
+
+	fd = open((char *)tree->right, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	ft_vector_add(fd_out, fd);
+	exec_args(tree->left);
+}
+
+int	exec_pipe(t_tree *tree, t_vector *fd_in, t_vector *fd_out)
 {
 	int	ret;
+	int	fd[2];
 
+	if (pipe(fd) == -1)
+		return (1);
+	ft_vector_add(fd_in, fd[0]);
+	ft_vector_add(fd_out, fd[1]);
 	exec_args(tree->left);
 	exec_args(tree->right);
 }
