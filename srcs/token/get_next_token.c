a0e09e05ae132d1369fd7e2d71d9bbe3951bfbd0
Loic Rio <lrio@student.42lyon.fr>
2024-02-29 03:30:37 +0100

    fix infinity loop whene &

--- srcs/token/get_next_token.c
+++ srcs/token/get_next_token.c
@@ -6,7 +6,7 @@
 /*   By: lrio <lrio@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 23:43:32 by lrio              #+#    #+#             */
-/*   Updated: 2024/02/28 23:51:21 by lrio             ###   ########.fr       */
+/*   Updated: 2024/02/29 03:29:47 by lrio             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 #include "token.h"
 #include "libft.h"
 
-static void	fill_token(char *str, t_token *token, size_t i, const t_token *type);
+static t_token fill_token(char *str, t_token *token, size_t i, const t_token *type);
 
 t_token	get_next_token(char *str)
 {
@@ -29,10 +29,7 @@ t_token	get_next_token(char *str)
 	while (i < 13)
 	{
 		if (0 == ft_strncmp(type[i].str, str, ft_strlen(type[i].str)))
-		{
-			fill_token(str, &token, i, type);
-			return (token);
-		}
+			return (fill_token(str, &token, i, type));
 		i++;
 	}
 	i = 0;
@@ -40,16 +37,19 @@ t_token	get_next_token(char *str)
 	str[i] != '>' && str[i] != ' ' && str[i] != '\t' && str[i] != '(' && \
 	str[i] != ')' && str[i] != '\'' && str[i] != '"')
 		i++;
+	if (i == 0 && str[i] && str[i + 1] != '&')
+		i++;
 	token.type = CHAR;
 	token.str = ft_strndup(str, i);
 	return (token);
 }
 
-static void	fill_token(char *str, t_token *token, size_t i, const t_token *type)
+static t_token fill_token(char *str, t_token *token, size_t i, const t_token *type)
 {
 	token->type = type[i].type;
 	if (type[i].type == QUOTE)
 		token->str = get_quote(str);
 	else
 		token->str = ft_strndup(str, ft_strlen(type[i].str));
+	return (*token);
 }
