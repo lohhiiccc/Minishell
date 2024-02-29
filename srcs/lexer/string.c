85bfa5459ed4ef299a9d7ccae84c45fae2d25cb1
Loic Rio <lrio@student.42lyon.fr>
2024-02-29 15:28:23 +0100

    fix (ls) pwd

--- srcs/lexer/string.c
+++ srcs/lexer/string.c
@@ -6,7 +6,7 @@
 /*   By: lrio <lrio@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 03:46:57 by lrio              #+#    #+#             */
-/*   Updated: 2024/02/29 04:36:45 by lrio             ###   ########.fr       */
+/*   Updated: 2024/02/29 15:27:31 by lrio             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -14,9 +14,16 @@
 
 int	lex_string(t_vector *vector, size_t i)
 {
-	int	j;
+	size_t			j;
+	t_token_type	last;
 
+	if (i > 0)
+		last = ((t_token *)vector->addr)[i - 1].type;
+	else
+		last = NONE;
 	j = 0;
+	if (last == PARENTESE_CL)
+		return (1);
 	while (((t_token *)vector->addr)[i].str[j])
 	{
 		if (((t_token *)vector->addr)[i].str[j] == '&')
