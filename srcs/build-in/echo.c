1220ac42b5a954069374b3bcc137686d0ac5a2d8
Michel Juffard <mjuffard@z1r6p3.42lyon.fr>
2024-03-01 15:11:50 +0100

    create build in echo

--- srcs/build-in/echo.c
+++ srcs/build-in/echo.c
@@ -3,9 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lrio <lrio@student.42lyon.fr>              +#+  +:+       +#+        */
+/*   By: mjuffard <mjuffard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 15:37:19 by lrio              #+#    #+#             */
-/*   Updated: 2024/02/29 15:37:19 by lrio             ###   ########.fr       */
+/*   Updated: 2024/03/01 14:47:52 by mjuffard         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_echo(char **arg)
+{
+	size_t	i;
+
+	i = 1;
+	if (arg[i] && !ft_strcmp(arg[i], "-n"))
+		i++;
+	while (arg[i])
+	{
+		ft_putstr_fd(arg[i], 1);
+		i++;
+	}
+	if (ft_strcmp(arg[1] && !arg[1], "-n"))
+		return (0);
+	write(1, "\n", 1);
+	return (0);
+}
