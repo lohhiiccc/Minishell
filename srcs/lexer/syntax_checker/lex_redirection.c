75a65a150bf0b48a4c4bb18c1fe952ac574a1fd7
Loic Rio <lrio@student.42lyon.fr>
2024-03-04 00:14:21 +0100

    remove wirte debug in return

--- srcs/lexer/syntax_checker/lex_redirection.c
+++ srcs/lexer/syntax_checker/lex_redirection.c
@@ -6,7 +6,7 @@
 /*   By: lrio <lrio@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 15:42:20 by lrio              #+#    #+#             */
-/*   Updated: 2024/03/04 00:01:35 by lrio             ###   ########.fr       */
+/*   Updated: 2024/03/04 00:14:03 by lrio             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "token.h"
@@ -20,7 +20,7 @@ unsigned char lex_redirect_in(t_vector *vector, size_t i)
 	else
 		type = T_NONE;
 	if (type == T_RED_IN || type == T_RED_OUT)
-		return (write(2, "test\n", 5), 1);
+		return (1);
 	return (0);
 }
 //> and >>
@@ -33,6 +33,6 @@ unsigned char lex_redirect_out(t_vector *vector, size_t i)
 	else
 		type = T_NONE;
 	if ( type == T_RED_OUT || type == T_RED_IN)
-		return (write(2, "test\n", 5), 1);
+		return (1);
 	return (0);
 }
