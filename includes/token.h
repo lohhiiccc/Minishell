e054b51f595929ccbc2c0e8df36411f34336d32f
Loic Rio <lrio@student.42lyon.fr>
2024-03-04 14:28:27 +0100

    update header

--- includes/token.h
+++ includes/token.h
@@ -6,7 +6,7 @@
 /*   By: lrio <lrio@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/28 23:47:51 by lrio              #+#    #+#             */
-/*   Updated: 2024/03/04 00:44:46 by lrio             ###   ########.fr       */
+/*   Updated: 2024/03/04 14:27:24 by lrio             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@
 
 typedef enum e_token_type
 {
-	T_NONE = 0,
-	T_CMD = 2,
-	T_PARENTHESE_OP = 3,
-	T_PARENTHESE_CL = 4,
-	T_RED_IN = 5,
-	T_RED_OUT = 6,
-	T_PIPE = 7,
-	T_LOGICAL_OP = 8,
-	T_IS_SPACE = 9,
-	T_FILES = 10
+	T_NONE,
+	T_CMD,
+	T_PARENTHESE_OP,
+	T_PARENTHESE_CL,
+	T_RED_IN,
+	T_RED_OUT,
+	T_PIPE,
+	T_LOGICAL_OP,
+	T_IS_SPACE,
+	T_FILES
 }	t_token_type;
 
 typedef struct s_token
