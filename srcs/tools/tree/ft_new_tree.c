291c020cc2dae23be64049502196da714c67bb11
Michel Juffard <mjuffard@z2r4p2.42lyon.fr>
2024-03-05 16:55:16 +0100

    add NULL to branch lef and right

--- srcs/tools/tree/ft_new_tree.c
+++ srcs/tools/tree/ft_new_tree.c
@@ -6,7 +6,7 @@
 /*   By: mjuffard <mjuffard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 17:41:16 by lrio              #+#    #+#             */
-/*   Updated: 2024/03/04 17:58:07 by mjuffard         ###   ########lyon.fr   */
+/*   Updated: 2024/03/05 16:53:46 by mjuffard         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,7 @@ t_tree	*ft_new_tree(void *content, t_node node)
 	new_tree = malloc(sizeof(t_tree));
 	new_tree->type = node;
 	new_tree->structur = content;
+	new_tree->left = NULL;
+	new_tree->right = NULL;
 	return (new_tree);
 }
