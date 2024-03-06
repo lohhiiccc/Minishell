36c37cfcb455d2c732d3a6455e66dbb35f6311bf
Loic Rio <lrio@student.42lyon.fr>
2024-03-06 15:10:29 +0100

    fix double free

--- srcs/lexer/launch_checker.c
+++ srcs/lexer/launch_checker.c
@@ -6,7 +6,7 @@
 /*   By: lrio <lrio@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/03 22:37:08 by lrio              #+#    #+#             */
-/*   Updated: 2024/03/04 00:50:32 by lrio             ###   ########.fr       */
+/*   Updated: 2024/03/06 15:07:14 by lrio             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,14 @@ unsigned char	syntax_check(t_vector *tokens)
 
 	i = 0;
 	ft_memset(parent, 0, sizeof(size_t) * 2);
-	while (i < tokens->nbr_elem)
+	while (i < tokens->nbr_elem - 1)
 	{
 		type = ((t_token *)tokens->addr)[i].type;
 		if (lex_global(tokens, i, parent, type))
 			return (1);
 		i++;
 	}
-	type = ((t_token *)tokens->addr)[tokens->nbr_elem - 1].type;
+	type = ((t_token *)tokens->addr)[tokens->nbr_elem - 2].type;
 	if (lex_last(parent, type))
 		return (1);
 	return (0);
