4c9cacdc5258da14222617d6cb9efe7e0ea67c08
Loic Rio <lrio@student.42lyon.fr>
2024-03-07 22:28:03 +0100

    change function prototype

--- srcs/tools/tree/make_node/command.c
+++ srcs/tools/tree/make_node/command.c
@@ -15,10 +15,10 @@
 #include "token.h"
 
 static void *free_command(t_cmd *content);
-static char	**get_cmd(t_vector *tokens);
-static size_t get_cmd_len(t_vector *tokens);
+static char	**get_cmd(t_token *tokens);
+static size_t get_cmd_len(t_token *tokens);
 
-t_tree	*make_command(t_vector *tokens)
+t_tree	*make_command(t_token *tokens)
 {
 	t_cmd		*content;
 
@@ -33,24 +33,24 @@ t_tree	*make_command(t_vector *tokens)
 	return (ft_new_tree(content, CMD));
 }
 
-static char	**get_cmd(t_vector *tokens)
+static char	**get_cmd(t_token *tokens)
 {
 	size_t	i;
 	size_t	len;
 	char 	**arg;
 
-	printf("%d\n", ((t_token *)tokens->addr)[0].type);
+	printf("%d\n", tokens[0].type);
 	arg = ft_calloc((get_cmd_len(tokens) + 1), sizeof(char *));
 	if (NULL == arg)
 		return (NULL);
 	i = 1;
-	arg[0] = ((t_token *)tokens->addr)[i].str;
+	arg[0] = tokens[i].str;
 	len = 1;
-	while (((t_token *)tokens->addr)[i].type != T_CMD && ((t_token *)tokens->addr)[i].type != T_NEWLINE)
+	while (tokens[i].type != T_CMD && tokens[i].type != T_NEWLINE)
 	{
-		if (((t_token *)tokens->addr)[i].type == T_ARG)
+		if (tokens[i].type == T_ARG)
 		{
-			arg[len] = ((t_token *)tokens->addr)[i].str;
+			arg[len] = tokens[i].str;
 			len++;
 		}
 		i++;
@@ -58,16 +58,16 @@ static char	**get_cmd(t_vector *tokens)
 	return (arg);
 }
 
-static size_t get_cmd_len(t_vector *tokens)
+static size_t get_cmd_len(t_token *tokens)
 {
 	size_t	len;
 	size_t	i;
 
 	i = 1;
 	len = 1;
-	while (((t_token *)tokens->addr)[i].type != T_CMD && ((t_token *)tokens->addr)[i].type != T_NEWLINE)
+	while (tokens[i].type != T_CMD && tokens[i].type != T_NEWLINE)
 	{
-		if (((t_token *)tokens->addr)[i].type == T_ARG)
+		if (tokens[i].type == T_ARG)
 			len++;
 		i++;
 	}
--- srcs/tools/tree/make_node/operator.c
+++ srcs/tools/tree/make_node/operator.c
@@ -6,25 +6,25 @@
 /*   By: lrio <lrio@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 18:58:06 by lrio              #+#    #+#             */
-/*   Updated: 2024/03/06 19:23:49 by lrio             ###   ########.fr       */
+/*   Updated: 2024/03/07 22:25:29 by lrio             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "tree.h"
 #include "token.h"
 
-static t_node get_operator(t_vector *token);
+static t_node get_operator(t_token *token);
 
-t_tree *make_operator(t_vector *tokens)
+t_tree *make_operator(t_token *tokens)
 {
 	return (ft_new_tree(NULL, get_operator(tokens)));
 }
 
-static t_node get_operator(t_vector *token)
+static t_node get_operator(t_token *token)
 {
-	if (((t_token *)token->addr)[0].type == T_LOGICAL_OP)
+	if (token[0].type == T_LOGICAL_OP)
 	{
-		if (((t_token *)token->addr)[0].str[0] == '&')
+		if (token[0].str[0] == '&')
 			return (O_AND);
 		else
 			return (O_OR);
