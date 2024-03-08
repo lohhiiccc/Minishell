1885a0d6db3251fc041edc68a8e9e9c227990452
Michel Juffard <mjuffard@z1r7p4.42lyon.fr>
2024-03-08 04:16:38 +0100

    update vector free

--- lib/libft/srcs/vector/ft_vector_free.c
+++ lib/libft/srcs/vector/ft_vector_free.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_vector_free.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lrio <lrio@student.42lyon.fr>              +#+  +:+       +#+        */
+/*   By: mjuffard <mjuffard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/05 23:49:24 by lrio              #+#    #+#             */
-/*   Updated: 2024/03/05 23:49:28 by lrio             ###   ########.fr       */
+/*   Updated: 2024/03/08 02:50:04 by mjuffard         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,13 @@ void ft_vector_free(t_vector *vector, void (* content_free)(void *))
 	size_t	i;
 
 	i = 0;
-	while (i < vector->nbr_elem)
+	if (content_free)
 	{
-		content_free(ft_vector_get(vector, i));
-		i++;
+		while (i < vector->nbr_elem)
+		{
+			content_free(ft_vector_get(vector, i));
+			i++;
+		}
 	}
 	free(vector->addr);
 }
