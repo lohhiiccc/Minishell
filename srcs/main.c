ff7de6af9fc6dc2a7c4b3e4fc0ca3442a125b658
Loic Rio <lrio@student.42lyon.fr>
2024-03-14 15:27:36 +0100

    "new main and promt"

--- srcs/main.c
+++ srcs/main.c
@@ -15,35 +15,26 @@
 #include "libft.h"
 #include "minishell.h"
 #include "env.h"
+#include "ft_printf.h"
 #include <stdlib.h>
 #include <stdio.h>
 
 int	main(int argc, char **argv, char **env)
 {
-	t_vector	*fd_in;
-	t_vector	*fd_out;
-	t_vector	*new_env;
-	// char	*temp;
-	// int i = 0;
+	t_vector	new_env;
 
 	(void)argc;
 	(void)argv;
-	fd_in = malloc(sizeof(t_vector));
-	fd_out = malloc(sizeof(t_vector));
-	new_env = malloc(sizeof(t_vector));
-	ft_vector_init(fd_in, sizeof(int));
-	ft_vector_init(fd_out, sizeof(int));
-	init_env(env, new_env);
-	// temp = ft_vector_get(new_env, i);
-	// while (temp)
-	// {
-	// 	printf("%s\n", temp);
-	// 	temp = ft_vector_get(new_env, ++i);
-	// }
-	while (1)
-		exec_args(prompt(new_env), fd_in, fd_out);
-	ft_vector_free(fd_in, NULL);
-	ft_vector_free(fd_out, NULL);
-	free(fd_in);
-	free(fd_out);
+	if (-1 == init_env(env, &new_env))
+	{
+		return (1);
+	}
+
+	while (prompt(&new_env))
+		;
+//	while (1)
+//		prompt(&new_env);
+
+//	free(fd_in);
+//	free(fd_out);
 }
