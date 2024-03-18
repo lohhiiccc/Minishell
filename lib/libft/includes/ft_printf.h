6b59b0326bcab560f3d5adae500bec7fc382caa0
Michel Juffard <mjuffard@z1r7p4.42lyon.fr>
2024-03-18 03:20:04 +0100

    change printf.h with ft_printf.h

--- lib/libft/includes/ft_printf.h
+++ lib/libft/includes/ft_printf.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.h                                           :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mjuffard <mjuffard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/17 15:37:33 by mjuffard          #+#    #+#             */
-/*   Updated: 2024/03/18 02:09:31 by mjuffard         ###   ########lyon.fr   */
+/*   Updated: 2024/03/18 02:38:41 by mjuffard         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PRINTF_H
-# define PRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 int		ft_dprintf(int fd, char *str, ...);
 int		ft_printf(char *str, ...);
