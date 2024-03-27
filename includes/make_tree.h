81148a888587ae478fdbdecd08e25b8a74a49dec
Loic Rio <lrio@student.42lyon.fr>
2024-03-27 14:42:36 +0100

    change unsigned char func to uint8_t

--- includes/make_tree.h
+++ includes/make_tree.h
@@ -14,12 +14,13 @@
 # define MAKE_TREE_H
 # include "tree.h"
 # include "token.h"
+# include <stdint.h>
 
 t_tree			*make_tree(t_token *tokens, t_vector *env);
 t_tree			*make_command(t_token *tokens, t_vector *env);
 t_tree			*make_operator(t_token *tokens);
 t_tree			*make_redirection(t_token *tokens);
 t_tree			*add_in_subtree(t_tree *root, t_tree *new);
-unsigned char	is_redirection(t_node root_type);
-unsigned char	is_operator(t_node root_type);
+uint8_t			is_redirection(t_node root_type);
+uint8_t			is_operator(t_node root_type);
 #endif
