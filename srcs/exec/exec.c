849ce02e9c7d3cf291a1edd2816c82faf5a59238
Michel Juffard <mjuffard@z2r4p4.42lyon.fr>
2024-03-29 12:49:33 +0100

    protect segfault with no cmd

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: mjuffard <mjuffard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 00:16:15 by mjuffard          #+#    #+#             */
-/*   Updated: 2024/03/19 17:18:48 by mjuffard         ###   ########lyon.fr   */
+/*   Updated: 2024/03/29 12:47:08 by mjuffard         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ int	exec_args(t_tree *tree, t_vector *fd_in, t_vector *fd_out, t_tree *root)
 		root = tree;
 	else
 		tree->root = root;
+	if (!tree)
+		return (0);
 	ret = tab[tree->type](tree, fd_in, fd_out);
 	return (ret);
 }
