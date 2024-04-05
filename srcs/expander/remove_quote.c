e8ff359bba0560c9d9b0c4627aa3fd35775d9c9a
Loic Rio <lrio@student.42lyon.fr>
2024-04-05 20:31:36 +0200

    fix quote removal

--- srcs/expander/remove_quote.c
+++ srcs/expander/remove_quote.c
@@ -10,13 +10,14 @@ void	remove_quote(char *s)
 	i = 0;
 	while (s[i])
 	{
-//		if (s[i] == '\'' || s[i] == '"')
-//		{
-//
-//		}
-		if (s[i] < 0)
+		if (s[i] == '\'' || s[i] == '"')
+		{
+			ft_memmove(s + i, s + i + 1, ft_strlen(s + i + 1) + 1);
+			continue;
+		}
+		else if (s[i] < 0)
 			s[i] = -s[i];
 		i++;
 	}
-
+	ft_printf("|%s|\n", s);
 }
