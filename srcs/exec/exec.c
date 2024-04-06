b9ff0dc0172e884b6f8c6c8e0e4408030a19b194
Michel Juffard <mjuffard@z1r6p5.42lyon.fr>
2024-04-06 16:37:29 +0200

    secure if no cmd in tree

--- srcs/exec/exec.c
+++ srcs/exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: mjuffard <mjuffard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/21 00:16:15 by mjuffard          #+#    #+#             */
-/*   Updated: 2024/04/05 00:58:22 by mjuffard         ###   ########lyon.fr   */
+/*   Updated: 2024/04/06 15:07:54 by mjuffard         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,12 @@ int	exec_args(t_tree *tree, t_vector *fd_in, t_vector *fd_out, t_tree *root)
 	const t_exec	tab[] = {&exec_or, &exec_and, &exec_pipe, &exec_here_doc,
 		&exec_apend, &exec_output, &exec_input, &exec_cmd};
 
+	if (!tree)
+		return (0);
 	if (!root)
 		root = tree;
 	else
 		tree->root = root;
-	if (!tree)
-		return (0);
 	ret = tab[tree->type](tree, fd_in, fd_out);
 	return (ret);
 }
