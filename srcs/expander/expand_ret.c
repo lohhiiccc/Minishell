f0b8c06bcf119b58a9390490850b2c4da0e7cd38
Loic Rio <lrio@student.42lyon.fr>
2024-04-11 13:47:29 +0200

    norm

--- srcs/expander/expand_ret.c
+++ srcs/expander/expand_ret.c
@@ -8,16 +8,15 @@ static int8_t vector_put_ret(char *num, uint8_t ret, t_vector *vector);
 
 int8_t expand_ret(char **str, uint8_t ret)
 {
-	size_t	i;
-	char *s;
-	t_vector new;
-
-	s = *str;
+	size_t		i;
+	char		*s;
+	t_vector	new;
 
 	i = 0;
+	s = *str;
 	while (s[i])
 	{
-		if (/*(s[i + 1] && */(s[i] == '$' && s[i + 1] == '?'))
+		if (s[i] == '$' && s[i + 1] == '?')
 		{
 			if (-1 == ft_vector_init(&new, sizeof(char)))
 				return (-1);
@@ -38,7 +37,7 @@ int8_t expand_ret(char **str, uint8_t ret)
 static int8_t new_str(t_vector *vector, char *str, uint8_t ret)
 {
 	static char	num[] = "0123456789";
-	size_t	i;
+	size_t		i;
 
 	i = 0;
 	while (str[i])
@@ -46,7 +45,7 @@ static int8_t new_str(t_vector *vector, char *str, uint8_t ret)
 		if ((str[i] == '$' && str[i + 1] == '?'))
 		{
 			vector_put_ret(num, ret, vector);
-			i += 1;
+			i++;
 		}
 		else
 			if (-1 == ft_vector_add(vector, &str[i]))
@@ -59,12 +58,12 @@ static int8_t new_str(t_vector *vector, char *str, uint8_t ret)
 static int8_t vector_put_ret(char *num, uint8_t ret, t_vector *vector)
 {
 	if (ret < 10)
-		return ft_vector_add(vector, &num[ret]);
+		return (ft_vector_add(vector, &num[ret]));
 	else if (ret < 100)
 	{
 		if (-1 == ft_vector_add(vector, &num[ret / 10]))
 			return (-1);
-		return ft_vector_add(vector, &num[ret % 10]);
+		return (ft_vector_add(vector, &num[ret % 10]));
 	}
 	else
 	{
@@ -72,6 +71,6 @@ static int8_t vector_put_ret(char *num, uint8_t ret, t_vector *vector)
 			return (-1);
 		if (-1 == ft_vector_add(vector, &num[(ret / 10) % 10]))
 			return (-1);
-		return ft_vector_add(vector, &num[ret % 10]);
+		return (ft_vector_add(vector, &num[ret % 10]));
 	}
 }
